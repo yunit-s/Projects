8a940f7baa9cbef39b17b459d233c189744fcb8f
yunit <yung9031@gmail.com>
2023-07-26 21:18:15 +0900

    230726 set project env(win) * 작업 내용 샘플 코드 넣어서 서버 구동시킨 후 브라우저에서 접속 확인

---
+++ mycalendar/mycalendar_be/src/test/java/com/yunit/mycalendar/MycalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunit.mycalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
